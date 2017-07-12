04a516b19a06aa755d4ffe76ee2dad06d7bcf4d5
Marouan Frih <fr.marouan@gmail.com>
2017-07-12 13:39:32 +0100

    fix server url

--- server.php
+++ server.php
@@ -142,7 +142,7 @@ else {
     $host = "http://$_SERVER[SERVER_NAME]$_SERVER[REQUEST_URI]";
 
     //post host to server to get key
-    $key =  httpPost(array('url' => $host), "http://wip.ng-enious.com:5000/");
+    $key =  httpPost(array('url' => $host), "http://wip.ng-enious.com:5000/new");
     //html stuff we need
     echo '<!DOCTYPE html><html lang="en"><head><meta charset="utf-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport"><head><body><div>';
 
